4075d7a0fe704e022229c23a306a06ff344456b2
SUFP6\Terry Nirv <anti_meson@hotmail.com>
2021-06-24 22:33:40 -0700

    modified password generator. changed file path of password database, added argparse to main.py,

--- main.py
+++ main.py
@@ -1,47 +1,81 @@
 # Python program to manage login passwords
 
+from sys import path
 import pyperclip
 import sqlite3
-from pw_gen import *
+from pw_gen import pwgen
+import tldextract
+import argparse
+
+print("***Python Password Manager***")
+db_path = r"d:\py_prjs\database\pwmngr.db"
+conn = sqlite3.connect(db_path)
+
+parse = argparse.ArgumentParser()
+parse.add_argument("--setup", action="store_true", help="Create table in database.")
+parse.add_argument("--debug", action="store_true", help="Insert test data table to debug database.")
+args = parse.parse_args()
+#######################################
+### data for debug only ###
+if args.debug:
+    conn.close()
+    conn = sqlite3.connect("pwmngr_debug.db")
+    curs = conn.cursor()
+    curs.execute("""
+        CREATE TABLE IF NOT EXISTS pwtable (
+        url text,
+        password text
+        )
+    """)
+    def inserts_db(list_in):
+        curs = conn.cursor()
+        curs.executemany("INSERT INTO pwtable VALUES (?,?)", list_in)
+        conn.commit()
+
+    list1 = [
+        ("testing.com","abc0000^<"),
+        ("company.com", "iopqr013^"),
+        ("reg.org", "masl+_14m"),
+        ("services.com", "@jalgiu1"),
+        ("shopping.com", "LE%)@Ndkf"),
+        ("mall.com", "PamylqOQ1*"),
+        ("special.org", "aloJ71"),
+        ("greetings.com", "pqmmnpas/"),
+    ]
+    inserts_db(list1)
+#######################################
 
 def show_all():
-    conn = sqlite3.connect("pwmngr.db")
     curs = conn.cursor()
     curs.execute("SELECT * FROM pwtable")
     data_row = curs.fetchall()
     print(*data_row,sep="\n")
     conn.commit()
-    conn.close()
 
 def search_db(url_in):
-    conn = sqlite3.connect("pwmngr.db")
     curs = conn.cursor()
-    curs.execute("SELECT * FROM pwtable WHERE url = (?)", (url_in,))
+    curs.execute("SELECT password FROM pwtable WHERE url = (?)", (url_in,))
     row = curs.fetchone()
     conn.commit()
-    conn.close()
-    if type(row) is not tuple:
-        return "NULL"
+    if not row:
+        return None
     else:
-        return row[1]
+        return row[0]
 
 def update_db(url_in, pw_in):
-    conn = sqlite3.connect("pwmngr.db")
     curs = conn.cursor()
     curs.execute("UPDATE pwtable SET password = (?) WHERE url = (?)", (pw_in, url_in))
     conn.commit()
-    conn.close()
 
-def update_retrieve(url_in):
-    pw_read = search_db(url_in)
+def update_retrieve():
     user_opt = " "
     while user_opt != "1" and user_opt != "2":
-        user_opt = input("Repeated record detected.\n(1)Retrieve password.\n(2)Update password.\n")
+        user_opt = input("Existing reecord found.\n(1)Retrieve password.\n(2)Update password.\n")
         if user_opt == "1":
-            pyperclip.copy(pw_read)
+            pyperclip.copy(s_result)
             print("Password copied to clipboard!")
         elif user_opt == "2":
-            new_pw = genpw()
+            new_pw = pwgen()
             update_db(url_read, new_pw)
             pyperclip.copy(new_pw)
             print(f"Password for {url_read} updated and copied to clipboard!\nPassword:||  {new_pw}  ||")
@@ -53,67 +87,43 @@ def delete_db():
     return
 
 def insert_db(url_in, pw_in):
-    conn = sqlite3.connect("pwmngr.db")
     curs = conn.cursor()
     curs.execute("INSERT INTO pwtable VALUES (?,?)", (url_in, pw_in))
     conn.commit()
-    conn.close()
 
-
-print("***Python Password Manager***")
-
-try:
-    conn = sqlite3.connect("pwmngr.db")
+if args.setup:
     curs = conn.cursor()
     curs.execute("""
-        CREATE TABLE pwtable (
-            url text,
-            password text
+        CREATE TABLE IF NOT EXISTS pwtable (
+        url text,
+        password text
         )
     """)
     conn.commit()
-    conn.close()
-except sqlite3.OperationalError:
-    pass
-
-#######################################
-### data for debug only ###
-# def inserts_db(list_in):
-#     conn = sqlite3.connect("pwmngr.db")
-#     curs = conn.cursor()
-#     curs.executemany("INSERT INTO pwtable VALUES (?,?)", list_in)
-#     conn.commit()
-#     conn.close()
-
-# list1 = [
-#         ("testing.com","abc0000^<"),
-#         ("company.com", "iopqr013^"),
-#         ("reg.org", "masl+_14m"),
-#         ("services.com", "@jalgiu1"),
-#         ("shopping.com", "LE%)@Ndkf"),
-#         ("mall.com", "PamylqOQ1*"),
-#         ("special.org", "aloJ71"),
-#         ("greetings.com", "pqmmnpas/"),
-# ]
-# inserts_db(list1)
-#######################################
 
 while True:
     try:
-        url_read = pyperclip.paste()    # search for record
-        ######################
-        url_read = "badtesting.com" # for debug ONLY!!!!
-        ######################
+        url_read = input("Enter url to search record/ press <enter> to read from clipboard:\n")
+        if url_read == "": url_read = pyperclip.paste()
+        url_extract = tldextract.extract(url_read)
+        url_read = url_extract.registered_domain
         s_result = search_db(url_read)
-        if s_result == "NULL": # new and create entry
-            user_opt = input("New web site detected.\n(1)Create new entry.\n(Otherwise)Search again.\n")
+        if not s_result and url_read != "" and "." in url_read: # new and create entry
+            user_opt = input(f"New web site detected: {url_read}\n(1)Create new entry, or <q> to quit.\n(Otherwise)Search again.\n")
             if user_opt == "1":
-                new_pw = genpw()
+                new_pw = pwgen()
                 insert_db(url_read,new_pw)
                 print(f"New entry created for {url_read}\nPassword:||  {new_pw}  ||")
                 break
+            elif user_opt == "Q" or user_opt == "q":
+                break
+        elif url_read == "" or "." not  in url_read:
+            print(f"Invalid URL detected: {url_read}")
+            pass
         else: # old record found
-            update_retrieve(url_read) # update or retrieve
+            update_retrieve() # update or retrieve
             break 
-    except:
-        pass
+    except Exception as err:
+        print("Exception: ", err)
+
+conn.close()
--- pw_gen.py
+++ pw_gen.py
@@ -4,13 +4,19 @@ import string
 from random import *
 
 # basic password generator
-def genpw():
+def pwgen_old():
     characters = string.ascii_letters + string.punctuation  + string.digits
     #print(characters)
     password =  "".join(choice(characters) for x in range(randint(8, 12)))
     return password
 
-# To do: add another *safe password generator
+# modified *safer password generator
 # contains 8 - 12 strings, with (1)caps, (2)small letters, (3) numbers, (4)1 or 2 punctuation
-# def genpw_safe():
-#     pw_length = [8,12]
+def pwgen():
+    pw_length = randint(10,14)
+    p_up = "".join(choice(string.ascii_uppercase) for x in range(randint(1,3)))
+    p_nums = "".join(choice(string.digits) for x in range(randint(2,3)))
+    p_punc = "".join(choice(string.punctuation) for x in range(randint(1,2)))
+    p_low = "".join(choice(string.ascii_lowercase) for x in range(pw_length-len(p_up)-len(p_nums)-len(p_punc)))
+    password = "".join(sample(p_up+p_low+p_nums+p_punc,pw_length))
+    return password
