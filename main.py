a878a4e3c3642bd5a1e21d38150e85e29db5502f
SUFP6\Terry Nirv <anti_meson@hotmail.com>
2021-06-27 18:06:35 -0700

    Implent of encryption. Master password is required. All password entries are encrypted before writen into the database.

--- main.py
+++ main.py
@@ -2,49 +2,48 @@
 
 import pyperclip
 import sqlite3
+
 from pw_gen import pwgen
 import tldextract
 import argparse
-
-from pw_encrypt import key_pw
+from shutil import copy2
+from pw_encrypt import key_read, master_pw, salt_gen, str_encrypt
+from os import remove
 
 from sys import platform
 if platform.startswith("linux"):
-    db_path = r"~/_database/pwmngr.db"
+    db_path = r"~/.database/pwmngr.db"
 elif platform.startswith("win32"):
-    db_path = r"d:\py_prjs\database\pwmngr.db"
+    db_path = r"d:\py_prjs\.database\pwmngr.db"
 conn = sqlite3.connect(db_path)
 
 parse = argparse.ArgumentParser()
-parse.add_argument("--setup", action="store_true", help="Create table in database.")
 parse.add_argument("--debug", action="store_true", help="Insert test data table to debug database.")
 args = parse.parse_args()
 #######################################
 ### data for debug only ###
 if args.debug:
     conn.close()
-    conn = sqlite3.connect("pwmngr_debug.db")
+    db_path = "pwmngr_debug.db"
+    conn = sqlite3.connect(db_path)
     curs = conn.cursor()
     curs.execute("""
         CREATE TABLE IF NOT EXISTS pwtable (
         url text,
-        password text
+        login text,
+        remark text,
+        password blob
         )
     """)
     def inserts_db(list_in):
         curs = conn.cursor()
-        curs.executemany("INSERT INTO pwtable VALUES (?,?)", list_in)
+        curs.executemany("INSERT INTO pwtable VALUES (?,?,?,?)", list_in)
         conn.commit()
 
     list1 = [
-        ("testing.com","abc0000^<"),
-        ("company.com", "iopqr013^"),
-        ("reg.org", "masl+_14m"),
-        ("services.com", "@jalgiu1"),
-        ("shopping.com", "LE%)@Ndkf"),
-        ("mall.com", "PamylqOQ1*"),
-        ("special.org", "aloJ71"),
-        ("greetings.com", "pqmmnpas/"),
+        ("abc.com", "name@email.com", "", str_encrypt("pw_test1", key_read(), "en")),
+        ("testing.com", "name@email.com", "hahaha", str_encrypt("pw_test2", key_read(), "en")),
+        ("school.org", "myidno", "some notes", str_encrypt("pw_test3", key_read(), "en")),
     ]
     inserts_db(list1)
 #######################################
@@ -56,81 +55,134 @@ def show_all():
     print(*data_row,sep="\n")
     conn.commit()
 
+def insert_db(url_in, login_in, remark_in, pw_in):
+    curs = conn.cursor()
+    curs.execute("INSERT INTO pwtable VALUES (?,?,?,?)", (url_in, login_in, remark_in, pw_in))
+    conn.commit()
+
 def search_db(url_in):
     curs = conn.cursor()
-    curs.execute("SELECT password FROM pwtable WHERE url = (?)", (url_in,))
+    curs.execute("SELECT login, remark, password FROM pwtable WHERE url = (?)", (url_in,))
     row = curs.fetchone()
     conn.commit()
     if not row:
         return None
     else:
-        return row[0]
+        search_result = {"login":row[0], "re":row[1], "pw":str_encrypt(row[2], key_read(), "de")}
+        return search_result
 
-def update_db(url_in, pw_in):
+def update_db(url_in, data_in, opt):
     curs = conn.cursor()
-    curs.execute("UPDATE pwtable SET password = (?) WHERE url = (?)", (pw_in, url_in))
+    if opt == "pw":
+        curs.execute("UPDATE pwtable SET password = (?) WHERE url = (?)", (data_in, url_in))
+    elif opt == "log":
+        curs.execute("UPDATE pwtable SET login = (?) WHERE url = (?)", (data_in, url_in))
+    elif opt == "re":
+        curs.execute("UPDATE pwtable SET remark = (?) WHERE url = (?)", (data_in, url_in))
     conn.commit()
 
 def update_retrieve():
     user_opt = " "
-    while user_opt != "1" and user_opt != "2":
-        user_opt = input("Existing reecord found.\n(1)Retrieve password.\n(2)Update password.\n")
+    while user_opt != "1" and user_opt != "2" and user_opt != "3":
+        print(f"Record found. Login ID: {s_result['login']}  (Note: {s_result['re']}).")
+        user_opt = input("(1)Retrieve password. (2)Generate new password & update the record. (3)Update database Login ID and note.==> ")
         if user_opt == "1":
-            pyperclip.copy(s_result)
+            pyperclip.copy(s_result["pw"])
             print("Password copied to clipboard!")
         elif user_opt == "2":
             new_pw = pwgen()
-            update_db(url_read, new_pw)
+            new_pw_enc = str_encrypt(new_pw, key_read(), "en")
+            update_db(domain, new_pw_enc, "pw")
             pyperclip.copy(new_pw)
-            print(f"Password for {url_read} updated and copied to clipboard!\nPassword:||  {new_pw}  ||")
+            print(f"Password for {domain} (Note: {s_result['re']}) updated and copied to clipboard!\nLogin & Password are: {s_result['login']}:|  {new_pw}  |")
+        elif user_opt == "3":
+            update_input = input("Input new Login ID and note (separated by ';'): ")
+            if update_input != "":
+                update_in = update_input.split(";", 1)
+                update_db(domain, update_in[0].strip(), "log")
+                if update_in[1]: update_db(domain, update_in[1].strip(), "re")
+                print(f"Database updated for {domain} (Note: {search_db(domain)['re']}), Login ID: {search_db(domain)['login']}")
         else:
-            print("Error, please enter 1 or 2.")
+            print("Error, please enter 1, 2 or 3.")
 
 def delete_db():
     # add code here
     return
 
-def insert_db(url_in, pw_in):
-    curs = conn.cursor()
-    curs.execute("INSERT INTO pwtable VALUES (?,?)", (url_in, pw_in))
-    conn.commit()
 
 print("***Python Password Manager***")
 
-if args.setup:
+curs = conn.cursor()
+curs.execute("SELECT count(name) FROM sqlite_master WHERE type= 'table' AND name='pwtable' ")
+if curs.fetchone()[0]==1:       # if pwtable exist -> use master to login
+    conn.commit()
+    master_pw("login")
+    if input("Press <enter> to continue, or <Ch> to change master password: ") == "Ch":
+        copy2(db_path, db_path+".old")
+        conn.close()
+        
+        conn = sqlite3.connect(db_path+".old")
+        curs = conn.cursor()
+        curs.execute("SELECT url, password FROM pwtable")
+        all_rows = curs.fetchall()
+        conn.commit()
+        all_dec = []
+        for i in all_rows:  # decrypt using existing key
+            data_de = str_encrypt(i[1], key_read(), "de")
+            all_dec += [(i[0], data_de)]     # i is a tuple, arbitrary assignment of its element is illegal
+        del all_rows
+        master_pw("change") # generate a new key using new master p/w
+        conn.close()
+        
+        conn = sqlite3.connect(db_path)
+        for i in all_dec:  # re-encrypt the p/w column using new m_pw
+            data_en = str_encrypt(i[1], key_read(), "en")
+            i = (i[0], data_en)
+            update_db(i[0], i[1], "pw")
+        del all_dec
+        conn.commit()
+        # master_pw("del_old")    # delete the key.old
+        # remove(db_path+".old")
+else:               # if no, create a salt then master password and store the key
+    salt_gen()
+    master_pw("new")
+    
     curs = conn.cursor()
     curs.execute("""
         CREATE TABLE IF NOT EXISTS pwtable (
         url text,
-        password text
+        login text,
+        remark text,
+        password blob
         )
     """)
     conn.commit()
 
-while True:
-    try:
-        url_read = input("Enter url to search record/ press <enter> to read from clipboard:\n")
+try:
+    while True:
+        url_read = input("Enter url to search record/ press <enter> to read from clipboard/ <q> to quit:\n")
+        if url_read == "q": break
         if url_read == "": url_read = pyperclip.paste()
         url_extract = tldextract.extract(url_read)
-        url_read = url_extract.registered_domain
-        s_result = search_db(url_read)
-        if not s_result and url_read != "" and "." in url_read: # new and create entry
-            user_opt = input(f"New web site detected: {url_read}\n(1)Create new entry, or <q> to quit.\n(Otherwise)Search again.\n")
+        domain = url_extract.registered_domain
+        if domain == "" or "." not in domain:
+            print(f"Invalid URL detected: {domain}")
+            continue
+        s_result = search_db(domain)
+        if not s_result: # new and create entry
+            user_opt = input(f"New web site detected: {domain}\n(1)Create new entry,\n(Otherwise)Search again.\n")
             if user_opt == "1":
+                new_login = input(f"Login id/email for {domain}: ")
+                new_remark = input(f"Additional note: ")
                 new_pw = pwgen()
                 pyperclip.copy(new_pw)
-                insert_db(url_read,new_pw)
-                print(f"New entry created for {url_read}\nPassword:||  {new_pw}  || copied to clipboard!")
-                break
-            elif user_opt == "Q" or user_opt == "q":
-                break
-        elif url_read == "" or "." not  in url_read:
-            print(f"Invalid URL detected: {url_read}")
-            pass
+                new_pw_enc = str_encrypt(new_pw, key_read(), "en")
+                insert_db(domain, new_login, new_remark, new_pw_enc)
+                print(f"New entry created for {domain}\nLogin & Password are: {new_login}:|  {new_pw}  | Password copied to clipboard!")
+            else: continue
         else: # old record found
             update_retrieve() # update or retrieve
-            break 
-    except Exception as err:
-        print("Exception: ", err)
+except Exception as err:
+    print("Exception: ", err)
 
 conn.close()
--- pw_encrypt.py
+++ pw_encrypt.py
@@ -3,33 +3,37 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
+from sys import platform
 import base64
-import os
+from os import remove, urandom
 from shutil import copy2
 
-# def salt_gen():
-#     salt_new = os.urandom(16)
-#     return salt_new
+def salt_gen():
+    salt_new = urandom(16)
+    with open(salt_path, "wb") as f:
+        f.write(salt_new)
 
-def key_write(key, key_file):
-    with open(key_file, "wb") as f:     # wb = write bytes
+def key_write(key):
+    with open(key_path, "wb") as f:     # wb = write bytes
         f.write(key)
     f.close()
 
-def key_read(key_file):
-    with open(key_file, "rb") as f:     # rb = read bytes
+def key_read():
+    with open(key_path, "rb") as f:     # rb = read bytes
         key = f.read()
     f.close()
     return key
 
-# def key_random():
-#     key = Fernet.generate_key()
-#     return key
+def key_random():
+    key = Fernet.generate_key()
+    return key
 
 def key_pw(pw_for_keygen):
     password = pw_for_keygen.encode()
-    salt = b'u\xbc\x17c\x8b\xff_\xc1\xc2go"\xa7\xa3}t'
+    with open(salt_path, "rb") as f:
+        salt = f.read()
+    f.close
+    # salt = b'u\xbc\x17c\x8b\xff_\xc1\xc2go"\xa7\xa3}t'
     kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             length=32,
@@ -51,42 +55,45 @@ def file_encrypt(file_in, file_out, key, opt):
     with open(file_out, "wb") as f: # Write the encrypted file
         f.write(output)
 
-# def str_encrypt(str_in, key, opt):
-#     fernet = Fernet(key)
-#     if opt == "en":
-#         str_out = fernet.encrypt(str_in.encode())
-#     elif opt == "de":
-#         str_out = fernet.decrypt(str_in).decode()
-#     return str_out
+def str_encrypt(str_in, key, opt):
+    fernet = Fernet(key)
+    if opt == "en":
+        str_out = fernet.encrypt(str_in.encode())
+    elif opt == "de":
+        str_out = fernet.decrypt(str_in).decode()
+    return str_out
 
-def master_pw():
-    while True:
+def master_pw(opt):
+    while opt == "login":
         key = key_pw(input("Enter Master Password to continue: "))
-        if key == key_read(key_path):
+        if key == key_read():
             print("Master Password correct!")
-            if input("Press <enter> to continue, or <C> to change master password.") == "C":
-                pw_in = input
-                key = key_pw(pw_in)
-                try:
-                    copy2(key_path, key_path+".old")      # backup the key file, write or overwrite key.old
-                except FileNotFoundError:
-                    print("Old key is not found.")
-                key_write(key, key_path)
-                break # or return ???
-            else: break
-        else: print("Incorrect password!")
-        
-# def master_pw_ch():
-#     if input("Press <enter> to continue, or <C> to change master password.") == "C":
-#         pw_in = input
-#         key = key_pw(pw_in)
-#         try:
-#             copy2(key_path, key_path+".old")      # backup the key file, write or overwrite key.old
-#         except FileNotFoundError:
-#             print("Old key is not found.")
-#         key_write(key, key_path)
-#     else: pass
+            break
+        else:
+            print("Incorrect password!")
+            continue
+    if opt == "new":
+        key = key_pw(input("Create a new Master Password: "))
+        key_write(key)
+    elif opt == "change":
+        pw_in = input("Enter a New Master Password: ")
+        key = key_pw(pw_in)
+        copy2(key_path, key_path+".old")
+        key_write(key)
+    elif opt == "del_old":
+        remove(key_path+".old")
 
-key_path = r"d:\py_prjs\_keys\pwmngr.key"
+if platform.startswith("linux"):
+    key_path = r"~/.keys/pwmngr.key"
+elif platform.startswith("win32"):
+    key_path = r"d:\py_prjs\.keys\pwmngr.key"
+salt_path = key_path.replace("pwmngr.key", "pwmngr.salt")
 
-# master_pw_ch("pw123")
+
+# salt_gen()
+# key_write(key_pw("pw123"))
+# for i in range(10):
+#     pw_test = "password"+str(i)
+#     print(str_encrypt(pw_test, key_read(), "en"))
+# s_enc = str_encrypt("testing123$|", key_read(), "en")
+# print(s_enc)
